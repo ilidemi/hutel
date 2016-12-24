65cd58c0be73bc6e4e4c25cb60916d11ebf39115
Ilia Demianenko <ilia.demianenko@gmail.com>
2016-12-24 11:58:23 +0530

    Save points to disk, r/w actions for all points

--- server/Controllers/PointsController.cs
+++ server/Controllers/PointsController.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using hutel.Filters;
 using hutel.Logic;
 using hutel.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
 
 namespace hutel.Controllers
 {
@@ -14,6 +17,8 @@ namespace hutel.Controllers
         private IMemoryCache _memoryCache;
         private const string _pointsKey = "points";
         private const string _tagsKey = "tags";
+        private const string _storagePath = ".\\server\\storage.json";
+        private const string _storageBackupPath = ".\\server\\storage.json.bak";
 
         public PointsController(IMemoryCache memoryCache)
         {
@@ -21,9 +26,10 @@ namespace hutel.Controllers
             Dictionary<Guid, Point> points;
             if (!_memoryCache.TryGetValue(_pointsKey, out points))
             {
+                points = ReadStorage();
                 _memoryCache.Set(
                     _pointsKey,
-                    new Dictionary<Guid, Point>(),
+                    points,
                     new MemoryCacheEntryOptions()
                         .SetPriority(CacheItemPriority.NeverRemove)
                 );
@@ -31,19 +37,59 @@ namespace hutel.Controllers
             Dictionary<string, Tag> tags;
             if (!memoryCache.TryGetValue(_tagsKey, out tags))
             {
+                tags = CreateTags();
                 _memoryCache.Set(
                     _tagsKey,
-                    CreateTags(),
+                    tags,
                     new MemoryCacheEntryOptions()
                         .SetPriority(CacheItemPriority.NeverRemove)
                 );
             }
         }
 
+        // GET /api/points
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var points = _memoryCache.Get<Dictionary<Guid, Point>>(_pointsKey);
+            return Json(points.Values);
+        }
+
+        // POST /api/points
+        [HttpPost]
+        [ValidateModelState]
+        public IActionResult PostAll([FromBody]List<Point> replacementPoints)
+        {
+            var points = _memoryCache.Get<Dictionary<Guid, Point>>(_pointsKey);
+            var tags = _memoryCache.Get<Dictionary<string, Tag>>(_tagsKey);
+            try
+            {
+                foreach (var point in replacementPoints)
+                {
+                    if (!tags.ContainsKey(point.TagId))
+                    {
+                        throw new ValidationException($"Unknown tag: {point.TagId}");
+                    }
+                    PointValidator.Validate(point, tags[point.TagId]);
+                }
+            }
+            catch(ValidationException ex)
+            {
+                return new BadRequestObjectResult(ex.ToString());
+            }
+            points.Clear();
+            foreach (var p in replacementPoints)
+            {
+                points.Add(p.Id, p);
+            }
+            WriteStorage(points);
+            return Json(points.Values);
+        }
+
         // POST api/points/id
         [HttpPost("{id}")]
         [ValidateModelState]
-        public IActionResult Post(Guid id, [FromBody]Point point)
+        public IActionResult PostOne(Guid id, [FromBody]Point point)
         {
             var points = _memoryCache.Get<Dictionary<Guid, Point>>(_pointsKey);
             var tags = _memoryCache.Get<Dictionary<string, Tag>>(_tagsKey);
@@ -60,7 +106,8 @@ namespace hutel.Controllers
                 return new BadRequestObjectResult(ex.ToString());
             }
             points[id] = point;
-            return Json(points);
+            WriteStorage(points);
+            return Json(points.Values);
         }
 
         private static Dictionary<string, Tag> CreateTags()
@@ -110,6 +157,35 @@ namespace hutel.Controllers
                     }
                 }
             };
+        }
+
+        private static Dictionary<Guid, Point> ReadStorage()
+        {
+            if (System.IO.File.Exists(_storagePath))
+            {
+                var pointsString = System.IO.File.ReadAllText(_storagePath);
+                return JsonConvert
+                    .DeserializeObject<List<Point>>(pointsString)
+                    .ToDictionary(point => point.Id, point => point);
+            }
+            else
+            {
+                return new Dictionary<Guid, Point>();
+            }
+        }
+
+        private static void WriteStorage(Dictionary<Guid, Point> points)
+        {
+            if (System.IO.File.Exists(_storageBackupPath))
+            {
+                System.IO.File.Delete(_storageBackupPath);
+            }
+            if (System.IO.File.Exists(_storagePath))
+            {
+                System.IO.File.Copy(_storagePath, _storageBackupPath);
+            }
+            var pointsJson = JsonConvert.SerializeObject(points.Values, Formatting.Indented);
+            System.IO.File.WriteAllText(_storagePath, pointsJson);
         }
     }
 }
