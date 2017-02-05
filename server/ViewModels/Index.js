a62dcdc5cea3f4bad535c47ac74d6401cad45a1a
Ilia Demianenko <belk94@gmail.com>
2017-02-04 16:00:51 -0800

    Inc/dec for point date input

--- server/ViewModels/Index.js
+++ server/ViewModels/Index.js
@@ -20,7 +20,7 @@ var app = new Vue({
       for (var field of tag.fields) {
         this.$set(this.tagInput, field.name, "");
       }
-      this.$set(this.tagInput, "date", "");
+      this.$set(this.tagInput, "date", moment().format('YYYY-MM-DD'));
       this.$set(this.tagInput, "tagId", tag.id);
       this.selectedTag = tag;
     },
@@ -28,14 +28,16 @@ var app = new Vue({
       this.selectedTag = null;
       this.tagInput = null;
     },
-    todayDate: function() {
-      var date = new Date();
-      var result = moment().format('YYYY-MM-DD');
+    incrementDate: function() {
+      var result = moment(this.tagInput.date)
+        .add(1, 'days')
+        .format('YYYY-MM-DD');
       this.$set(this.tagInput, "date", result);
     },
-    yesterdayDate: function() {
-      var date = new Date();
-      var result = moment().subtract(1, 'days').format('YYYY-MM-DD');
+    decrementDate: function() {
+      var result = moment(this.tagInput.date)
+        .subtract(1, 'days')
+        .format('YYYY-MM-DD');
       this.$set(this.tagInput, "date", result);
     },
     submit: function() {
