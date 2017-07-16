485649bb746515b734a4c70e78ed5ea30d73c661
Ilia Demianenko <ilia.demianenko@gmail.com>
2017-07-16 11:39:26 -0700

    Minor React fixes

--- server/ViewModels/App.js
+++ server/ViewModels/App.js
@@ -12,48 +12,42 @@ import Home from './Home';
 
 injectTapEventPlugin();
 
-class App extends React.Component {
-  constructor(){
-    super();
+const App = () => {
+  const theme = {
+    headerBackground: Colors.deepPurple900,
+    headerText: Colors.fullWhite,
+    topBackground: Colors.fullWhite,
+    historyBackground: Colors.grey100,
+    historyDateText: Colors.deepPurple900
   }
-
-  render() {
-    const theme = {
-      headerBackground: Colors.deepPurple900,
-      headerText: Colors.fullWhite,
-      topBackground: Colors.fullWhite,
-      historyBackground: Colors.grey100,
-      historyDateText: Colors.deepPurple900
+  const muiTheme = getMuiTheme({
+    palette: {
+      primary1Color: Colors.amber900,
+      accent1Color: Colors.yellow500
     }
-    const muiTheme = getMuiTheme({
-      palette: {
-        primary1Color: Colors.amber900,
-        accent1Color: Colors.yellow500
-      }
-    });
-    const columnStyle = {
-      maxWidth: 800,
-      margin: "auto",
-      display: "flex",
-      flexDirection: "column",
-      flexHeight: "100%",
-      flexMinHeight: "100%"
-    };
-    return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <Paper
-          style={columnStyle}
-          zDepth={5}
-        >
-          <div>
-            <Route exact path="/" render={(props) => (
-              <Home {...props} theme={theme} />
-            )} />
-          </div>
-        </Paper>
-      </MuiThemeProvider>
-    );
-  }
-}
+  });
+  const columnStyle = {
+    maxWidth: 800,
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    flexHeight: "100%",
+    flexMinHeight: "100%"
+  };
+  return (
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <Paper
+        style={columnStyle}
+        zDepth={5}
+      >
+        <div>
+          <Route exact path="/" render={(props) => (
+            <Home {...props} theme={theme} />
+          )} />
+        </div>
+      </Paper>
+    </MuiThemeProvider>
+  );
+};
 
 export default App;
