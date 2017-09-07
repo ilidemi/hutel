3b2f7815d85ff6af761e3ced9483398f171b3ee7
Ilia Demianenko <ilia.demianenko@gmail.com>
2017-09-07 09:00:58 -0700

    Collapse the list of tags

--- server/ViewModels/SelectTag.js
+++ server/ViewModels/SelectTag.js
@@ -6,8 +6,27 @@ import FontIcon from 'material-ui/FontIcon';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class SelectTag extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false
+    };
+  }
+
   selectTag(tagId) {
     this.props.selectTag(tagId);
+  }
+
+  expand() {
+    this.setState({
+      isExpanded: true
+    });
+  }
+  
+  collapse() {
+    this.setState({
+      isExpanded: false
+    });
   }
 
   render() {
@@ -33,9 +52,9 @@ class SelectTag extends React.Component {
       const labelStyle = {
         paddingLeft: 6
       }
-      var buttons = this.props.tags.map(tag => (
+      var toButton = tag => (
         <RaisedButton
-          key={tag.id}
+          key={'+' + tag.id}
           onClick={this.selectTag.bind(this, tag.id)}
           label={tag.id}
           labelStyle={labelStyle}
@@ -49,7 +68,38 @@ class SelectTag extends React.Component {
               add
             </FontIcon>}
         />
-      ));
+      );
+      var expandButton =
+        <RaisedButton
+          key="expand"
+          onClick={this.expand.bind(this)}
+          primary={true}
+          style={buttonStyle}
+          icon={
+            <FontIcon
+              className="material-icons"
+              style={iconStyle}
+            >
+              expand_more
+            </FontIcon>}
+        />
+      var collapseButton =
+        <RaisedButton
+          key="collapse"
+          onClick={this.collapse.bind(this)}
+          primary={true}
+          style={buttonStyle}
+          icon={
+            <FontIcon
+              className="material-icons"
+              style={iconStyle}
+            >
+              expand_less
+            </FontIcon>}
+        />
+      var buttons = this.state.isExpanded
+        ? this.props.tags.map(toButton).concat([collapseButton])
+        : this.props.tags.slice(0, 10).map(toButton).concat([expandButton]);
       return (
         <div style={style}>
           {buttons}
